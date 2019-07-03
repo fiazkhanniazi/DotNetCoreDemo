da3c1ad2a799fc148ee09a56ef2b18162fefd63b
fiazkhanniazi <fiazkhanniazi@gmail.com>
2019-07-03 18:45:43 +0500

    1.25

--- WebApplicationDocker/Controllers/ValuesController.cs
+++ WebApplicationDocker/Controllers/ValuesController.cs
@@ -13,7 +13,7 @@ namespace WebApplicationDocker.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "Dev ops hackathone development demo apply command latest version 1.24" };
+            return new string[] { "Dev ops hackathone development demo apply command latest version 1.25" };
         }
 
         // GET api/values/5
