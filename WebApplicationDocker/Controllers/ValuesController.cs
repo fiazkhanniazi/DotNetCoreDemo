6e184933f53e516bd09ccd28ada85173756be031
fiazkhanniazi <fiazkhanniazi@gmail.com>
2019-07-03 14:46:08 +0500

    test

--- WebApplicationDocker/Controllers/ValuesController.cs
+++ WebApplicationDocker/Controllers/ValuesController.cs
@@ -13,7 +13,7 @@ namespace WebApplicationDocker.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "Dev ops hackathone development demo apply command" };
+            return new string[] { "Dev ops hackathone development demo apply command latest" };
         }
 
         // GET api/values/5
